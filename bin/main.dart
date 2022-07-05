93c3d865cf0807e537bf971cfbee83cd2360c242
Rustambek <rustambeksafarovv@gmail.com>
2022-07-05 14:03:22 +0500

    Redone

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,11 @@
 // babyloniam method
 // create function named func with arguments s and d
 // The variables are S and d given
-double func(s,d){
-    int a = 0;
-    int b = 0;
+double func(S,d){
+    double a = 0;
+    double b = 0;
     a = (S-(d*d))/(2*d);
-    b = a + d;
+    b = d + a;
     double x = b - ((a*a)/(2*b));
     return x;
 }
